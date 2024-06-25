0784437e301ae36241e4295f9e2aad1001d577af
Shachar Mossek <shachar.mossek@gmail.com>
2024-06-25 20:38:38 +0300

    Set vite base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://webiks.github.io/svg-example/',
   plugins: [react(), svgr()],
 })
